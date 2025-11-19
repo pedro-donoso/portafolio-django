4de29111c83c97d3c37aafc9a28c09696d3ff4df
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-19 13:57:10 -0300

    creo urls

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,28 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from productos import views
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Productos
+    path('', views.lista_productos, name='lista_productos'),
+    path('productos/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+    path('productos/agregar/', views.agregar_producto, name='agregar_producto'),
+    path('productos/<int:pk>/editar/', views.editar_producto, name='editar_producto'),
+    path('productos/<int:pk>/eliminar/', views.eliminar_producto, name='eliminar_producto'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Autenticación
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/registro/', views.registro, name='registro'),
 ]
+
+# Servir archivos media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
